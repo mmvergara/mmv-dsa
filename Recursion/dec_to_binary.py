0af019fde99e55dce7403a18101a16c379e9037a
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-06 16:21:32 +0800

    update

--- Recursion/dec_to_binary.py
+++ Recursion/dec_to_binary.py
@@ -1,17 +1,27 @@
-def dec_to_binary(num:int,result="")->str:
-    if num == 0:
-        return result
-    result = str(num % 2 ) + result
-    return dec_to_binary(int(num / 2), result)
+# def dec_to_binary(num:int,result="")->str:
+#     if num == 0:
+#         return result
+#     result = str(num % 2 ) + result
+#     return dec_to_binary(num // 2, result)
 
-def dec_to_binary_nr(num:int,result="")->str:
-    result = ""
-    while(num != 0):
-      if(num % 2 == 0):
-          result = "0" + result
-      else:
-          result = "1" + result
-      num = int(num/2)
-    return result
+def dec_to_binary_count_1(num:int,count=0)->str:
+    if num == 0:
+        return count
+    if num % 2 == 1:
+        count+=1
+    return dec_to_binary_count_1(num // 2, count)
+
+# def dec_to_binary_nr(num:int,result="")->str:
+#     result = ""
+#     while(num != 0):
+#       if(num % 2 == 0):
+#           result = "0" + result
+#       else:
+#           result = "1" + result
+#       num = int(num/2)
+#     return result
         
-print(dec_to_binary_nr(233))
+print(dec_to_binary_count_1(15))
+
+# 15 -4
+# 5 - 2
---
+++ leetcode/338.py
@@ -0,0 +1,17 @@
+
+
+def countBits(self, n: int) -> list[int]:
+    def dec_to_binary_count_1(num:int,count=0)->str:
+      if num == 0:
+          return count
+      if num % 2 == 1:
+          count+=1
+      return dec_to_binary_count_1(num // 2, count)
+    out_arr = []
+    for i in range(n+1):
+        out_arr.append(dec_to_binary_count_1(i))
+    return out_arr
+    
+
+
+print(countBits("",2))
