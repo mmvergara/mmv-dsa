65b4e896a6e4238f4923e7b74bcec73a9e5af670
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-09 06:00:54 +0800

    update

---
+++ 392.py
@@ -0,0 +1,11 @@
+def isSubsequence(self, s: str, t: str) -> bool:
+    if s == "":
+        return True 
+    i = 0
+    for c in t:
+        if c == s[i]:
+            i+=1
+            if i >= len(s):
+                return True
+            
+    return False
