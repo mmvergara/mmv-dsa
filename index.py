6eae7ce235048c539ba37025d958f2a4383d00e4
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-09 12:13:28 +0800

    update

--- index.py
+++ index.py
@@ -1,2 +1,26 @@
-new = (0,1)
-print(sum(new))
+arr = [1,2,3,4,5]
+target = 9
+
+
+# def twoSum(arr:list,target:int):
+#     keepLoop = True
+#     for n in arr:
+#         if not keepLoop:
+#             break
+#         for x in arr:
+#             if n+x == target:
+#                 print("found it ")
+#                 keepLoop = False
+#                 break
+
+def twoSum(arr:list,target:int):
+    complements = {}
+    for i,n in enumerate(arr):
+        complement = target - n
+        print(complements)
+        if complement in complements:
+            return [i,complements[complement]]
+        complements[n] = i
+    print("NOT FOUND")
+
+print(twoSum(arr,target))
