5eb5643fd2813de419f5e4c89a031ec752763083
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-19 08:44:15 +0800

    update

---
+++ yo.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  string firstname; //variable created as a string
+  cout << "What's your first name\n";
+  cin >> firstname; //asking for the users' first name
+  cout << "Hello \n" << firstname;
+  return 0;
+}
