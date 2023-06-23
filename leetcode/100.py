8ac79379f7b6e4f871ccfd8824d4d0e4837f90d7
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-23 19:57:45 +0800

    update

--- leetcode/100.py
+++ leetcode/100.py
@@ -1,12 +1,14 @@
 # Definition for a binary tree node.
-from typing import Optional
-
+from dsa import *
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+# Perform dfs on both tree's while comparing each values to each other
+# if p q is None means that we already are at the leaf and we did not encounter any mismatch
+# if p.val != q.val base case is the comparison part where we compare the values of the tree
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         def dfs_on_two(p,q):
--- leetcode/101.py
+++ leetcode/101.py
@@ -20,13 +20,17 @@ root.right = TreeNode(2)
 root.right.left = TreeNode(3)
 root.right.right = TreeNode(4)
 
-
 def isSymmetric(self, root: Optional[TreeNode]) -> bool:
     def dfs(left, right):
+        # If there are no more subtrees to compare then we traversed the 2 tree's without encountering some mismatch
         if left is None and right is None:
-            return True
+            return 
+        
+        # if left has value and right has none or vice versa therefore they are not the same
         if left is None or right is None:
             return False
+        
+        # recursive part where we check the values if they are the same and their subtrees
         return (
             left.val == right.val
             and dfs(left.left, right.right)
