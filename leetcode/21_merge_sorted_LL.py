5600f53582b1fd32b8f681cbf80d21b0e56eaf30
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-23 19:30:13 +0800

    update

--- leetcode/21_merge_sorted_LL.py
+++ leetcode/21_merge_sorted_LL.py
@@ -1,32 +1,29 @@
-# Definition for singly-linked list.
-from typing import Optional
+from dsa import *
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    head = ListNode()
+    current = head
 
-        head = ListNode()
-        current = head
-        while list1 is not None and list2 is not None:
-            if list1.val < list2.val:
-                current.next = list1
-                current = current.next
-                list1 = list1.next
-            else:
-                current.next = list2
-                current = current.next
-                list2 = list2.next
-
-        while list1 is not None:
+    # is it just like the merging part of the merge sort
+    # loop through both of the list at the same time comparing each values
+    while list1 is not None and list2 is not None:
+        if list1.val < list2.val:
             current.next = list1
             current = current.next
             list1 = list1.next
-
-        while list2 is not None:
+        else:
             current.next = list2
             current = current.next
             list2 = list2.next
-        return head.next
+
+    # if one of the list are empty we can add the remaining elements in the LL
+    while list1 is not None:
+        current.next = list1
+        current = current.next
+        list1 = list1.next
+
+    while list2 is not None:
+        current.next = list2
+        current = current.next
+        list2 = list2.next
+    return head.next
