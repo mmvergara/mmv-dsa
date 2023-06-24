26ab0df90127e05054687173293a4430a406f7c4
mmvergara <mark.jesusmanabat@gmail.com>
2023-06-24 14:47:54 +0800

    update

--- leetcode/21.py
+++ leetcode/21.py
@@ -4,6 +4,7 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
     head = ListNode()
     current = head
 
+
     # is it just like the merging part of the merge sort
     # loop through both of the list at the same time comparing each values
     while list1 is not None and list2 is not None:
@@ -15,6 +16,7 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
             current.next = list2
             current = current.next
             list2 = list2.next
+
 
     # if one of the list are empty we can add the remaining elements in the LL
     while list1 is not None:
