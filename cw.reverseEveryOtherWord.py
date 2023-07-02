19491c8f2a677ccd306dfb038d3630d5c217b77c
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-02 16:43:15 +0800

    update

--- cw.reverseEveryOtherWord.py
+++ cw.reverseEveryOtherWord.py
@@ -1,4 +1,5 @@
 def reverse_alternate(s:str):
+    # if odd reverse the string, else dont
     return " ".join([x if i % 2 == 0 else x[::-1] for i,x in enumerate(s.split())])
 
 print(reverse_alternate("WHAT YOWW "))
--- cw_pigIt.py
+++ cw_pigIt.py
@@ -1,17 +1,13 @@
 import string
 
-p = "[" in list(string.punctuation)
-print(p)
+
 def pig_it(text):
     arr = text.split()
-    
-    for i in range(len(arr)):
 
+    for i in range(len(arr)):
         arr[i] = f"{arr[i][1:]+arr[i][:1]}ay"
-    
-    return (" ".join(arr))
-    
+
+    return " ".join(arr)
 
 
 pig_it("Pig latin is cool")
-
