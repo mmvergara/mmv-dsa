e7dd5f6f10f4677fd311202f9dcf8387b3e5d88b
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-11 08:15:41 +0800

    update

---
+++ graph.py
@@ -0,0 +1,33 @@
+g = {
+    "A": ["B", "C"],
+    "B": ["A", "D", "E"],
+    "C": ["A", "F"],
+    "D": ["B", "Z"],
+    "E": ["B", "F"],
+    "F": ["C", "E"],
+    "Z": [],
+} 
+
+visited = set()
+
+
+def dfs(graph, source, target, path: list):
+    path.append(source)
+    if source == target:
+        return path.copy()
+
+    if source in visited:
+        return None
+    visited.add(source)
+
+    for n in graph[source]:
+        res = dfs(graph, n, target, path.copy())
+        if res is not None:
+            return res
+
+    return "WOW"
+
+
+res = dfs(g, "A", "ZG", [])
+print(res)
+print("done")
---
+++ hr_migratoryBirds.py
@@ -0,0 +1,20 @@
+
+def migratoryBirds(arr):
+    mp = {}
+    f = [0] * 6
+    for n in arr:
+        f[n]+=1
+
+    maxf = 0
+    for i in range(len(f)): 
+        if f[maxf] < f[i]:
+            maxf = i
+    print(maxf)
+    return []
+
+
+
+    # Write your code here
+
+res = migratoryBirds([1,2,3,4,5,4,3,2,1,3,4])
+print(res)
