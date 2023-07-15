689cadae2df87aa9adf3a4752106fbdb27f91552
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-15 10:02:42 +0800

    update

---
+++ 39.py
@@ -0,0 +1,17 @@
+from dsa import *
+
+def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    res = []
+    def gen(target, k, candidates, curCan):
+        if target == 0:
+            res.append(curCan[:])
+        if target < 0:
+            return
+        for i in range(k, len(candidates)):
+            remainder = target - candidates[i]
+            gen(remainder, i, candidates, curCan + [candidates[i]])
+    gen(target, 0, candidates, [])
+    return res
+
+
+print(combinationSum("", [2,3,5], 8))
