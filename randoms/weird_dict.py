d256b90960cfb29806d486f843afa79064cd89bb
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-21 07:13:58 +0800

    update

--- randoms/weird_dict.py
+++ randoms/weird_dict.py
@@ -1,8 +1,6 @@
 a = 1
 b = 2
 
-d = {a<b:'smaller than', a==b:'equal to', a>b: 'greater than'}
+d = {a < b: "smaller than", a == b: "equal to", a > b: "greater than"}
 
-print(d[a<b])
-
-
+print(d[a < b])
