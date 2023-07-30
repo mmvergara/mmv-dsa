8bbac2a03b8af34ae16e2d09cfff5ddcb0b914a3
mmvergara <mark.jesusmanabat@gmail.com>
2023-07-30 09:07:19 +0800

    update

--- leetcode/explained/3.py
+++ leetcode/explained/3.py
@@ -1,29 +1,19 @@
-# https://leetcode.com/problems/longest-substring-without-repeating-characters/
 def lengthOfLongestSubstring(self, s: str) -> int:
-    if len(s) == 0:
+    n = len(s)
+    if n == 0:
         return 0
-    subStr = ""
-    maxSubStr = 0 
-    for i in range(len(s)):
-        if subStr == "":
-            subStr += s[i]
-            maxSubStr+=1
-            continue
-        if s[i] in subStr:
-            subStr = subStr[subStr.rindex(s[i])+1:]
-            subStr += s[i]
-        else:
-            subStr += s[i]
-        if maxSubStr < len(subStr):
-            print(subStr)
-            maxSubStr = len(subStr) 
-    return maxSubStr
 
-        
+    maxL = 0
+    mp = {}
+    l = 0
 
-# print(lengthOfLongestSubstring("","abcabcbb")) # 3
-print(lengthOfLongestSubstring("","dvdf")) # 3
-# print(lengthOfLongestSubstring("","aabaab!bb")) # 1'
+    for right in range(n):
+        if s[right] in mp and mp[s[right]] >= l:
+            l = mp[s[right]] + 1
+        mp[s[right]] = right
+        maxL = max(maxL, right - l + 1)
+    print(maxL)
+    return maxL
 
-# strr = "dvdf"
-# print(strr[strr.rindex("d")+1:])
+
+lengthOfLongestSubstring("", "abba")
