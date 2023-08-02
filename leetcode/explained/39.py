975f0791a40df3733afd992b85c1864c61a5e474
mmvergara <mark.jesusmanabat@gmail.com>
2023-08-02 16:27:45 +0800

    update

--- leetcode/explained/39.py
+++ leetcode/explained/39.py
@@ -1,7 +1,9 @@
 from dsa import *
 
+
 def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
     res = []
+
     def gen(target, k, candidates, curCan):
         if target == 0:
             res.append(curCan[:])
@@ -10,7 +12,9 @@ def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         for i in range(k, len(candidates)):
             remainder = target - candidates[i]
             gen(remainder, i, candidates, curCan + [candidates[i]])
+
     gen(target, 0, candidates, [])
     return res
 
-print(combinationSum("", [2,3,5], 8))
+
+print(combinationSum("", [2, 3, 5], 8))
