11b60fe3359a5ff669a83e4743a2ee50a4c21df8
mmvergara <mark.jesusmanabat@gmail.com>
2023-08-04 05:25:12 +0800

    update

---
+++ 191.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        if n == 0:
+            return n
+        def rec(n, c=0):
+            if n == 1:
+                return c
+            if n % 2 == 1:
+                c += 1
+
+            return rec(n // 2, c)
+
+        return rec(n) + 1
