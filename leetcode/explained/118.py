879b773a3d279796e208ae9ea8835341897063a2
mmvergara <mark.jesusmanabat@gmail.com>
2023-08-07 16:53:44 +0800

    update

--- leetcode/explained/118.py
+++ leetcode/explained/118.py
@@ -1,16 +1,20 @@
-# Generates Pascal triangle
+from dsa import *
+
+# learned: for list that we know that are going to be filled with n values, we can optimize more by initializing an array with ex.[None for _ in range(i + 1)]
+
+
 def generate(self, numRows: int) -> list[list[int]]:
-    out = [[1]]
-    for i in range(numRows-1):
-        newRow = [1]
-        for j in range(i):
-            newRow.append(out[i][j]+out[i][j+1])
-        newRow.append(1)
-        out.append(newRow)
-    return out
+    if numRows == 0:
+        return []
+    ans = [[1]]
+    for i in range(1, numRows):
+        last = ans[-1]
+        new = [None for _ in range(i + 1)]
+        new[0] = new[-1] = 1
+        for j in range(1, i):
+            new[j] = last[j - 1] + last[j]
+        ans.append(new)
+    return ans
 
 
-
-
-res = generate("",5)
-print(res)
+print(generate("", 5))
