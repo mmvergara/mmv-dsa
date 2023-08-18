00bb1798643de71d7060add965ed74b049daea7f
mmvergara <mark.jesusmanabat@gmail.com>
2023-08-18 19:14:22 +0800

    update

---
+++ 1876.py
@@ -0,0 +1,13 @@
+def solve(s):
+    count = 0 
+    for i in range(len(s)-2):
+        if s[i] != s[i+1] and s[i+1] != s[i+2] and s[i+2] != s[i]:
+            count+=1
+    return count
+        
+
+
+
+print(solve("xyzzaz"))
+print(solve("aababcabc"))
+
---
+++ 1935.py
@@ -0,0 +1,13 @@
+def solve(text,brokenletters):
+    st = set(brokenletters)
+    count = 0
+    for strr in text.split(" "):
+        
+
+    return count
+
+
+
+
+
+print(solve("leet code","ld"))
