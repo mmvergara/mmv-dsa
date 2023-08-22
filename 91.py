f9355017056d06a6024c39ea0c52b3e69aa3fca2
mmvergara <mark.jesusmanabat@gmail.com>
2023-08-22 17:39:27 +0800

    update

---
+++ 91.py
@@ -0,0 +1,37 @@
+def numDecodings(self, s: str) -> int:
+    if len(s) == 0 or s[0] == "0":
+        return 0
+    combs = []
+
+    def rec(combs, s, curS, i):
+        if i >= len(s):
+            combs.append(curS[:])
+            return
+
+        # either take one or take two
+        rec(combs, s, curS + [s[i]], i + 1)
+
+        if i < len(s) - 1:
+            rec(combs, s, curS + [s[i] + s[i + 1]], i + 2)
+
+    rec(combs, s, [], 0)
+    print(combs)
+    res = 0
+    for comb in combs:
+        print(f"for {comb}")
+        add = 1
+        for i in comb:
+            if i[0] == "0":
+                add = 0
+                break
+            i = int(i)
+            if i <= 0 or i > 26:
+                add = 0
+                break
+        print(f"added {add}")
+        res += add
+    print(res)
+    return res
+
+
+numDecodings("", "1111111111111111111111111111")
---
+++ leetcode/explained/56.py
@@ -0,0 +1,30 @@
+from dsa import *
+
+
+def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    intervals = sorted(intervals)
+    print(intervals)
+    if len(intervals) <= 1:
+        return intervals
+
+    res = []
+    last = None
+    for i in range(len(intervals)):
+        current = intervals[i]
+        if last is None:
+            last = current
+            continue
+
+        if current[0] <= last[1]:
+            last = [last[0], max(current[1], last[1])]
+            continue
+        else:
+            res.append(last)
+            last = current
+    if last:
+        res.append(last)
+
+    return res
+
+
+print(merge("", [[1, 4], [2, 3]]))
