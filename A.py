18e1f27c47d7eaa0e6eebfa16071364900141cae
mmvergara <mark.jesusmanabat@gmail.com>
2023-09-04 07:41:12 +0800

    update

--- A.py
+++ A.py
@@ -1,6 +1,1 @@
-x = 0
 
-for i in range(5):
-    x += 1
-
-print(x)
--- C.cpp
+++ C.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include 
 using namespace std;
 
+
 int main() {
-	float i = floor1952%10;
-	cout << i;
-	return 0 ;
+	
+
+	return 0;
 }
