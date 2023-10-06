059e91675396719e59d5543303a1c3e497e1351a
mmvergara <mark.jesusmanabat@gmail.com>
2023-10-07 02:11:42 +0800

    update

--- C.cpp
+++ C.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <vector>
+#include <unordered_map>
+
 
 using namespace std;
 
+void changeNum (int *x){
+	*x = 10;
+}
+
 int main() {
-	int num = 2932;
-	int ans = INT_MAX;
-	for ( int m = 1 ; m < num; m*=10){
-		int a = num % m;
-		int b = num / m;
-		cout << a << "-" << b << endl;
-		int sum =  ( num % m ) + (num / m );
-		cout << sum << endl;
-		ans = min(ans,sum);
-	}
-	cout << ans;
+	int num = 1;
+	changeNum(&num);
+	cout << num;
+	
+	
 	return 0;
 }
